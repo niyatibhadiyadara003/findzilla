c64ea09212019b4150e8f2f6c106b18ddd2dbb00
RupeshBrainbinary <104359322+RupeshBrainbinary@users.noreply.github.com>
2023-04-09 14:05:22 +0530

    change faq

---
+++ lib/screen/privacy_policy/privacy_policy.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jobseek/common/widgets/backButton.dart';
+import 'package:jobseek/utils/color_res.dart';
+
+class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: InkWell(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: backButton(),
+                ),
+              ),
+              const SizedBox(width: 46),
+              const Text(
+                "Privacy Policy",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    height: 1,
+                    color: ColorRes.black),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text("")
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screen/terms_condition/terms_condition.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jobseek/common/widgets/backButton.dart';
+import 'package:jobseek/utils/color_res.dart';
+
+class TermsAndServicesScreen extends StatelessWidget {
+  TermsAndServicesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: InkWell(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: backButton(),
+                ),
+              ),
+              const SizedBox(width: 46),
+              const Text(
+                "Terms & Condition",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    height: 1,
+                    color: ColorRes.black),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Text("")
+        ],
+      ),
+    );
+  }
+}
